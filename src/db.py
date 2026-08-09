252b1bf963fa9e253ca8bb935ac4530d41569387
yeongchan <ychan@yeongchanui-MacBookAir.local>
2026-08-09 15:34:36 +0900

    fix: Cloud SQL Unix 연결

--- src/db.py
+++ src/db.py
@@ -5,14 +5,14 @@ from sqlalchemy import create_engine
 
 DB_HOST = os.getenv("DB_HOST", "postgres")
 DB_NAME = os.getenv("DB_NAME", "oneulro")
+DB_USER = os.getenv("DB_USER", "oneulro")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", ""))
+DB_PORT = os.getenv("DB_PORT", "5432")
 
-_DATABASE_URL = "postgresql://{user}:{password}@{host}:{port}/{name}".format(
-    user=os.getenv("DB_USER", "oneulro"),
-    password=quote_plus(os.getenv("DB_PASSWORD", "")),
-    host=DB_HOST,
-    port=os.getenv("DB_PORT", "5432"),
-    name=DB_NAME,
-)
+if DB_HOST.startswith("/cloudsql/"):
+    _DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={DB_HOST}"
+else:
+    _DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 try:
     engine = create_engine(_DATABASE_URL, pool_pre_ping=True)
